20b950a717e6516d7167a0c8240c126b3951f8ef
Sam <mozafari.sam@gmail.com>
2017-01-16 13:38:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,35 +4,22 @@ $(document).ready(function() {
     $(window).scrollTo(this.hash, 1000);
   });
 
-  $("#text1").typeIt({
-    speed: 50,
+  $("#hometitle").typeIt({
+    speed: 75,
     autoStart: false,
-    lifeLike: true,
-    deleteSpeed: 100,
   })
-  .tiType("hello world")
-  .tiPause(1000)
-  .tiType(", my name is <strong>sam pour mozafari</strong>")
-  .tiPause(1000)
-  // .tiBreak()
-  // .tiPause(250)
-  .tiBreak()
-  .tiPause(250)
-  .tiType("a frontend developer ")
-  // .tiPause(250)
-  .tiType("and ")
-  .tiBreak()
-  .tiType("graphic designer ")
-  // .tiPause(250)
-  .tiType("from copenhagen")
-  .tiPause(300)
-  .tiType(".")
-  .tiPause(300)
-  .tiType(".")
-  .tiPause(300)
-  .tiType(".");
-  // .tiPause(250)
-  // .tiBreak()
-  // .tiType("<a href='index.html#about'>...read more</a>");
+  .tiType("Hello world");
+
+  $("#projectstitle").typeIt({
+    speed: 75,
+    autoStart: false,
+  })
+  .tiType("Projects");
+
+  $("#learningtitle").typeIt({
+    speed: 75,
+    autoStart: false,
+  })
+  .tiType("Learning");
 
 });
